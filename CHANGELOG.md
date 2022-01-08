f00658a137170ac3b0e7340d664b6138cbea5dfd
Jason Chen <chezbgone@gmail.com>
2022-01-07 23:38:33 -0500

    Implement main functionality

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for filtag
 
+## 0.2.0.0 -- Fri Jan  7 11:24:49 PM EST 2022
+
+* Implement main functionality.
+
 ## 0.1.0.0 -- Sun Oct 31 05:41:33 PM EDT 2021
 
 * First version. Released on an unsuspecting world.
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
 import Control.Monad
+import Data.Bifunctor
 import qualified Data.ByteString as B
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -10,51 +12,110 @@ import qualified Data.Map as Map
 import Data.Maybe
 import Data.Serialize
 import Data.Time
+import PyF
 import System.Directory
 import System.Environment
 import GHC.Generics
 import Control.Monad.Except
 
-type TagMap = Map FilePath (Set String)
+type Tag = String
+type TagMap = Map Tag (Set FilePath)
 
 data MapReadError = UnreadableError | NonexistentError
-  deriving Generic
+  deriving (Generic, Show)
 instance Serialize MapReadError
 
 tagFile :: FilePath
 tagFile = ".tags"
 
-addFile :: FilePath -> TagMap -> Maybe TagMap
-addFile file m = do
-  guard $ Map.member file m
-  pure $ Map.insert file Set.empty m
+addTags :: Set Tag -> FilePath -> TagMap -> TagMap
+addTags tags file tagMap = Set.foldr' upd tagMap tags
+  where upd :: Tag -> TagMap -> TagMap
+        upd tag tm = Map.alter add tag tm
+        add Nothing = Just $ Set.singleton file
+        add (Just files) = Just $ Set.insert file files
 
-getTagMap :: IO (Either MapReadError TagMap)
-getTagMap = undefined
-
-getTagMap' :: ExceptT MapReadError IO TagMap
-getTagMap' = do
+readTags :: ExceptT MapReadError IO TagMap
+readTags = do
   exists <- lift $ doesFileExist tagFile
-  pure _
+  if exists then do
+    bs <- lift $ B.readFile tagFile
+    liftEither $ first (const UnreadableError) $ decode bs
+  else throwError NonexistentError
 
-writeTagMap :: TagMap -> IO ()
-writeTagMap tm = undefined
+writeTags :: TagMap -> IO ()
+writeTags tm = do
+  exists <- doesFileExist tagFile
+  when exists $ do
+    writePerms <- writable <$> getPermissions tagFile
+    guard writePerms
+  B.writeFile tagFile (encode tm)
+
+moveOldTags :: IO ()
+moveOldTags = do
+  timeStr <- formatTime defaultTimeLocale "%Y%m%d%H%M%S"
+             <$> (utcToLocalZonedTime =<< getCurrentTime)
+  putStrLn [fmt|Moving old {tagFile} file to {tagFile}{timeStr}|]
+  renameFile tagFile (tagFile ++ timeStr)
+
+newTags :: IO TagMap
+newTags = do
+  putStrLn [fmt|Creating new {tagFile} file|]
+  pure Map.empty
+
+help :: String
+help = "filtag - file tagging cli\n" ++
+       "------\n" ++
+       "filtag list [verbose]         Lists available tags\n" ++
+       "filtag find {tag}             Lists files associated with [tag]\n" ++
+       "filtag tag {file} {tags}      Add [tags] to [file]\n" ++
+       "filtag remove {file} {tags}   Add [tags] to [file]"
 
 main :: IO ()
 main = do
   args <- getArgs
-  tagMap <- getTagMap
-  case args of
-    ["add", file] -> do
-      putStrLn $ unwords ["adding", file]
+  tm <- runExceptT readTags
+  tagMap <- case tm of
+    Left UnreadableError  -> moveOldTags >> pure Map.empty
+    Left NonexistentError -> pure Map.empty
+    Right tm              -> pure tm
+  tagMap' <- case args of
     "tag" : file : tags -> do
-      putStrLn $ unwords $ ["tagging", file, "with"] ++ tags
-      putStrLn "not supported yet"
+      pure $ Just $ addTags (Set.fromList tags) file tagMap
     ["list"] -> do
-      putStrLn "listing"
-      putStrLn "not supported yet"
+      let tags = map (second Set.size) $ Map.toList tagMap
+      mapM_ (putStrLn . \(t, n) -> [fmt|{t} ({n})|]) tags
+      pure Nothing
     ["find", tag] -> do
-      putStrLn $ unwords ["finding", tag]
-      putStrLn "not supported yet"
-    _ -> putStrLn "command not recognized"
-  print args
+      case Map.lookup tag tagMap of
+        Nothing     -> pure ()
+        Just files' -> mapM_ putStrLn files'
+      pure Nothing
+    ["remove", file] -> do
+      pure $ Just $ Map.mapMaybe upd tagMap
+        where upd fs = let fs' = Set.delete file fs
+                       in if Set.null fs' then Nothing else Just fs'
+    "remove" : file : tags -> do
+      pure $ Just $ Map.mapMaybeWithKey upd tagMap
+        where upd t fs
+                | t `elem` tags = let fs' = Set.delete file fs
+                    in if Set.null fs' then Nothing else Just fs'
+                | otherwise     = Just fs
+    ["list", "verbose"] -> do
+      forM_ (Map.toList tagMap) $ \(tag, fs) -> do
+        putStrLn [fmt|{tag} |]
+        forM_ (Set.toList fs) $ \file ->
+          putStrLn [fmt|  {file}|]
+      pure Nothing
+    [] -> do
+      putStrLn help
+      pure Nothing
+    ["help"] -> do
+      putStrLn help
+      pure Nothing
+    args' -> do
+      putStrLn [fmt|unrecognized command "{unwords args}"|]
+      pure Nothing
+  case tagMap' of
+    Just tm -> writeTags tm
+    Nothing -> pure ()
